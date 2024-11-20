87d177fef92fdf551f45d2cdafa6834726025536
andrenur <andrenurrohman@gmail.com>
2024-11-20 16:10:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 # main.py
+import sys
+import os
+
+# Add the src directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
+
 from src import (
     AirportDepartureTerminal,
     passenger,
